63cc0d2dba29b62b19e38314ee809ad6326a25d8
Soohoon Lee <shlee@nvista.co.kr>
2019-06-27 18:24:27 +0900

    Initial commit

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,15 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        final EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        final EntityManager em = emf.createEntityManager();
+        em.close();
+        emf.close();
+    }
+}
