3e405a1a214d8d4255fbe3cd1b93c5aab494cc2d
soohoonlee <jjinimania@gmail.com>
2019-07-12 01:47:16 +0900

    영속성 컨텍스트 2

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,7 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
@@ -9,7 +10,22 @@ public class JpaMain {
     public static void main(String[] args) {
         final EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         final EntityManager em = emf.createEntityManager();
-        em.close();
+
+        final EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            final Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZ");
+
+            System.out.println("================================");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
     }
 }
